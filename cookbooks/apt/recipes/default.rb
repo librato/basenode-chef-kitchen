6e62b67626686321777d69f7c1b0c6a36ddfa5d1
Mike Heffner <mikeh@fesnel.com>
2012-08-31 17:05:32 -0400

    Import cookbooks/apt version fe00aebdabef7cc45e333003c0f7eba8d4dcbbc1

--- cookbooks/apt/recipes/default.rb
+++ cookbooks/apt/recipes/default.rb
@@ -23,12 +23,6 @@ execute "apt-get-update" do
   command "apt-get update"
   ignore_failure true
   not_if do ::File.exists?('/var/lib/apt/periodic/update-success-stamp') end
-end
-
-# For other recipes to call to force an update
-execute "apt-get update" do
-  command "apt-get update"
-  ignore_failure true
   action :nothing
 end
 
