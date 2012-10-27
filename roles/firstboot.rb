aaa1fee96423726b04fa91db956344832d4d38e0
Mike Heffner <mikeh@fesnel.com>
2012-10-26 23:45:43 -0400

    Include the collectd attributes in the first boot role.

--- roles/firstboot.rb
+++ roles/firstboot.rb
@@ -2,6 +2,11 @@ name "firstboot"
 description "Run at first boot to configure node"
 
 attrs = {
+  :collectd => {
+    :use_fqdn => false,
+    :hostname => '%%HOSTNAME%%',
+    :interval => 60
+  },
 }
 
 default_attributes(attrs)
