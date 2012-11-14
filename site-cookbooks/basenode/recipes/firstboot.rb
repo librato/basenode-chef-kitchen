2f275b21c9e9925d8648de8def62eb671aed5f65
Mike Heffner <mikeh@fesnel.com>
2012-11-13 19:10:02 -0500

    Grab unique instance IDs in the basenode recip.

--- site-cookbooks/basenode/recipes/firstboot.rb
+++ site-cookbooks/basenode/recipes/firstboot.rb
@@ -11,8 +11,36 @@ myaz = %x{curl -s http://169.254.169.254/latest/meta-data/placement/availability
 
 Chef::Log.info("userdata == #{userdata.inspect}")
 
-inst = userdata[:instance] || 0
-nodename = "#{userdata[:node_name]}-#{inst}"
+nodename = ""
+
+# Try to acquire a new unique instance ID
+if userdata[:uniq_name_gen]
+  ruby_block do
+    block do
+      broker_id = nil
+      1.upto(3) do
+        cmd = "/usr/local/bin/uniq_name_gen.py"
+        nspace = userdata[:uniq_name_gen][:namespace]
+        key = userdata[:uniq_name_gen][:access_key]
+        skey = userdata[:uniq_name_gen][:secret_key]
+        broker_id = %x{#{cmd} #{nspace} #{key} #{skey}}.chomp
+        unless $?.success?
+          broker_id = nil
+          Chef::Log.warn("Failed to acquire a unique instance ID...sleeping for 30 secs")
+          # Randomize sleep time in case of racing
+          sleep 20 + rand(10)
+        end
+      end
+
+      if broker_id
+        Chef::Log.info "Got an instance ID of #{broker_id}"
+        node[:uniq_instance_id] = broker_id
+        nodename = "#{userdata[:node_name]}_#{broker_id}"
+      end
+    end
+    only_if {!userdata[:uniq_name_gen].nil? && node[:uniq_instance_id].nil?}
+  end
+end
 
 # Update rsyslog name (papertrail)
 bash "update_rsyslog_name" do
