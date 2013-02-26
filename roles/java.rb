a26bfd159ccc122873e333c5c3c7088ea52c70d8
Mike Heffner <mikeh@fesnel.com>
2013-02-26 11:17:52 -0500

    Upgrade JDK version.

--- roles/java.rb
+++ roles/java.rb
@@ -7,7 +7,7 @@ default_attributes({
                        :jdk => {
                          "6" => {
                            :x86_64 => {
-                             :url => "http://s3.amazonaws.com/librato_download/chef/jdk-6u30-linux-x64.bin"
+                             :url => "http://s3.amazonaws.com/librato_download/chef/jdk-6u41-linux-x64.bin"
                            }
                          }
                        }
