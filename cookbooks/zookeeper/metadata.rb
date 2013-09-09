ce8e6bf9ee360fe00aa6bf7730485a3b9d38de30
lucky <luckythetourist@gmail.com>
2013-09-09 10:44:14 -0700

    Add zookeeper as a generic role/service Use Zookeeper 3.4.5 (up from 3.3.x) Add *.swp to .gitignore'

---
+++ cookbooks/zookeeper/metadata.rb
@@ -0,0 +1,17 @@
+maintainer       "Librato"
+maintainer_email "jared@librato.com"
+license          "Apache 2.0"
+description      "Installs/Configures zookeeper"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.6"
+
+supports         "ubuntu"
+
+# XXX: Java 7?
+depends          "java"
+depends          "iptables"
+depends          "runit"
+
+# XXX: ZK Client?
+recipe           "zookeeper::client", "Installs ZK client"
+recipe           "zookeeper::server", "Installs ZK server"
